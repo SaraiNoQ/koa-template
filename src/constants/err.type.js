65e4cdc1c6e109b1d93a9264616688332a0c3179
SaraiNoQ <2994498974@qq.com>
2022-08-09 23:51:24 +0800

    feat: add token support

--- src/constants/err.type.js
+++ src/constants/err.type.js
@@ -4,6 +4,10 @@ module.exports = {
 		code: '1452',
 		message: 'DB_REFERENCE_ERROR',
 	},
+	invalidToken: {
+		code: '1901',
+		message: 'INVALID_TOKEN',
+	},
 	testBussinessError: {
 		code: '10001',
 		message: 'test backend error!',
